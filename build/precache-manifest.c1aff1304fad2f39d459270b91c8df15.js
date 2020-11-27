743ebf44c7f8325bd505549b8765fe4c8a9b97dc
Quent1125 <62792412+Quent1125@users.noreply.github.com>
2020-11-27 17:22:54 +0100

    Add files via upload

---
+++ build/precache-manifest.c1aff1304fad2f39d459270b91c8df15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5852e8968c0b57284d6f76727865f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e5cf5ffd587135f3e4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "33ae501919f2e6303ff8",
+    "url": "/static/css/main.47fbd698.chunk.css"
+  },
+  {
+    "revision": "12e5cf5ffd587135f3e4",
+    "url": "/static/js/2.032bf3ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.032bf3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ae501919f2e6303ff8",
+    "url": "/static/js/main.50122899.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  }
+]);
